d4877e2c1312f78fe8d8e7176de72aff797455d7
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-09-23 13:38:31 +0700

    [Improvement] Add playholder when loading image

--- app/src/main/java/vn/linh/vqherokuapp/feature/user/adapter/viewholder/ItemViewHolder.kt
+++ app/src/main/java/vn/linh/vqherokuapp/feature/user/adapter/viewholder/ItemViewHolder.kt
@@ -3,12 +3,13 @@ package vn.linh.vqherokuapp.feature.user.adapter.viewholder
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import kotlinx.android.synthetic.main.item_user_item.view.*
+import vn.linh.vqherokuapp.R
 import vn.linh.vqherokuapp.extension.loadImage
 import vn.linh.vqherokuapp.feature.user.adapter.model.ItemItem
 
 class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(item: ItemItem) {
-        itemView.image_item.loadImage(item.image)
+        itemView.image_item.loadImage(item.image, R.drawable.ic_cat_playholder)
     }
 }
--- app/src/main/java/vn/linh/vqherokuapp/feature/user/adapter/viewholder/UserViewHolder.kt
+++ app/src/main/java/vn/linh/vqherokuapp/feature/user/adapter/viewholder/UserViewHolder.kt
@@ -3,14 +3,14 @@ package vn.linh.vqherokuapp.feature.user.adapter.viewholder
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import kotlinx.android.synthetic.main.item_user.view.*
+import vn.linh.vqherokuapp.R
 import vn.linh.vqherokuapp.extension.loadImage
 import vn.linh.vqherokuapp.feature.user.adapter.model.UserItem
 
 class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(user: UserItem) {
-        // todo add place holder
-        itemView.image_avatar.loadImage(user.image,-1,true)
+        itemView.image_avatar.loadImage(user.image, R.drawable.ic_cat_playholder,true)
         itemView.text_name.text = user.name
     }
 }
